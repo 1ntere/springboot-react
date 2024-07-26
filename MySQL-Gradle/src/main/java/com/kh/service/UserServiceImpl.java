f6cb5cf5cb1f812857bd2add4505aff794afb6e5
1ntere <kwonhc9337java@gmail.com>
2024-07-26 09:34:09 +0900

    0725 MySQL 백엔드 서버 (Gradle)

---
+++ MySQL-Gradle/src/main/java/com/kh/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.kh.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.dto.User;
+import com.kh.mapper.UserMapper;
+
+@Service
+//서비스 기능을 상세하게 작성해주는 공간
+public class UserServiceImpl implements UserService {
+	@Autowired
+	UserMapper userMapper;
+	
+	@Override
+	public List<User> findAll() {
+		return userMapper.findAll();
+	}
+	
+	@Override
+	public void insertUser(User user) {
+		userMapper.insertUser(user);
+		//★ void와 return은 공존 불가능
+	}
+}
